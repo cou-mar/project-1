c1b05249071933c3d348eae360d22ee81741336c
Courtney Marasigan <coumardev@gmail.com>
2022-11-02 13:39:24 -0400

    added boolean & point values

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext('2d');
 let scoreDisplay = document.querySelector('#score');
 let scoreValue = 0;
 
+let winElement = document.querySelector('#winner');
+let loseElement = document.querySelector('#loser');
+
 class Player {
     constructor(x, y, width, height){
         this.x = x;
@@ -67,94 +70,128 @@ let trashArr = [];
 let banana = new Image();
 banana.src = "./media/banana peel.png"
 banana.arrId = "banana"
+banana.recyclable = false;
+banana.points = -5;
 trashArr.push(banana);
 
 let can = new Image();
 can.src = "./media/can.png"
 can.arrId = "can"
+can.recyclable = true;
+can.points = 1;
 trashArr.push(can);
 
 let cardboard = new Image();
 cardboard.src = "./media/cardboard.png"
 cardboard.arrId = "cardboard"
+cardboard.recyclable = true;
+cardboard.points = 1;
 trashArr.push(cardboard);
 
 let cerealBox = new Image();
 cerealBox.src = "./media/cereal box.png"
 cerealBox.arrId = "cerealBox"
+cerealBox.recyclable = true;
+cerealBox.points = 1;
 trashArr.push(cerealBox);
 
 let chineseTakeout = new Image();
 chineseTakeout.src = "./media/chinese takeout.png"
 chineseTakeout.arrId = "chineseTakeout"
+chineseTakeout.recyclable = false;
+chineseTakeout.points = -5;
 trashArr.push(chineseTakeout);
 
 let coffeeCup = new Image();
 coffeeCup.src = "./media/coffee cup.png"
 coffeeCup.arrId = "coffeeCup"
+coffeeCup.recyclable = false;
+coffeeCup.points = -5;
 trashArr.push(coffeeCup);
 
 let cupHolder = new Image();
 cupHolder.src = "./media/cup holder.png"
 cupHolder.arrId = "cupHolder"
+cupHolder.recyclable = true;
+cupHolder.points = 1;
 trashArr.push(cupHolder);
 
 let paperTowel = new Image();
 paperTowel.src = "./media/paper towel roll.png"
 paperTowel.arrId = "paperTowel"
+paperTowel.recyclable = true;
+paperTowel.points = 1;
 trashArr.push(paperTowel);
 
 let paper = new Image();
 paper.src = "./media/paper.png"
 paper.arrId = "paper"
+paper.recyclable = true;
+paper.points = 1;
 trashArr.push(paper);
 
 let pizzaBox = new Image();
 pizzaBox.src = "./media/pizza box.png"
 pizzaBox.arrId = "pizzaBox"
+pizzaBox.recyclable = false;
+pizzaBox.points = -5;
 trashArr.push(pizzaBox);
 
 let plasticBag = new Image();
 plasticBag.src = "./media/plastic bag.png"
 plasticBag.arrId = "plasticBag"
+plasticBag.recyclable = false;
+plasticBag.points = -5;
 trashArr.push(plasticBag);
 
 let plasticBottle = new Image();
 plasticBottle.src = "./media/plastic bottle.png"
 plasticBottle.arrId = "plasticBottle"
+plasticBottle.recyclable = true;
+plasticBottle.points = 1;
 trashArr.push(plasticBottle);
 
 let plasticTakeout = new Image();
 plasticTakeout.src = "./media/plastic takeout.png"
 plasticTakeout.arrId = "plasticTakeout"
+plasticTakeout.recyclable = false;
+plasticTakeout.points = -5;
 trashArr.push(plasticTakeout);
 
 let plasticUtensils = new Image();
 plasticUtensils.src = "./media/plastic utensils.png"
 plasticUtensils.arrId = "plasticUtensils"
+plasticUtensils.recyclable = false;
+plasticUtensils.points = -5;
 trashArr.push(plasticUtensils);
 
 let styrofoamTakeout = new Image();
 styrofoamTakeout.src = "./media/styrofoam takeout.png"
 styrofoamTakeout.arrId = "styrofoamTakeout"
+styrofoamTakeout.recyclable = false;
+styrofoamTakeout.points = -5;
 trashArr.push(styrofoamTakeout);
 
 let styrofoam = new Image();
 styrofoam.src = "./media/styrofoam.png"
 styrofoam.arrId = "styrofoam"
+styrofoam.recyclable = false;
+styrofoam.points = -5;
 trashArr.push(styrofoam);
 
-// console.log(trashArr, "Trash Array")
+// console.log(trashArr, 'Trash Array')
+// console.log(recyclableArr, 'Recyclable Array')
 
 class FallingItems {
-    constructor(){
+    constructor(recyclable, points){
         this.x = Math.random() * 1051;
         this.y = 0;
         this.velocity = Math.ceil(Math.random() * 3);
         this.width = 100;
         this.height = 90;
-        this.trash = trashArr[Math.floor(Math.random() * trashArr.length)]
-
+        this.allItems = trashArr[Math.floor(Math.random() * trashArr.length)]
+        this.recyclable = this.allItems.recyclable
+        this.points = this.allItems.points
     }
     moveDown(){
         this.y += this.velocity;
@@ -178,7 +215,7 @@ class FallingItems {
         }
     }
     draw(){
-        ctx.drawImage(this.trash, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.allItems, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -219,11 +256,9 @@ const animationLoop = () => {
     clearCanvas();
 
     if (frameCount % 60 == 0){
-        // 
+        
         addItems();
 
-        //bads drop on odd numbers
-        //else if goods drop on even numbers
     }
     for (let i = 0; i < itemsArr.length; i++){
         // drop trash from sky
